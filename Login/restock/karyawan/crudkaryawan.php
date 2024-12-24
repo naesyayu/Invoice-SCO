823d95ce7cc15b1e0ce5b26d66470d8dcfe799f2
sheshh <anggitanajwa2007@gmail.com>
2024-12-24 09:44:09 +0700

    Update crudkaryawan.php

--- Login/restock/karyawan/crudkaryawan.php
+++ Login/restock/karyawan/crudkaryawan.php
@@ -61,6 +61,12 @@ if (isset($_GET['delete'])) {
                     <span>Invoice</span>
                 </a>
             </li>
+            <li>
+                <a href="rekapitulasiharian.php">
+                    <i class="fas fa-align-justify"></i>
+                    <span>Rekap</span>
+                </a>
+            </li>
         </ul>
         <ul class="logout">
             <li>
